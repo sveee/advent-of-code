2c4f4a72a3c2a07588d6ecea0bae55e20a476555
sve <>
2023-12-02 08:07:34 +0200

    refactor

--- aoc/2023/02.py
+++ aoc/2023/02.py
@@ -3,7 +3,7 @@ import numpy as np
 from aoc.utils import get_input
 
 text = get_input(day=2, year=2023)
-minimum_cubes = []
+required_cubes = []
 for line in text.splitlines():
     game, cubes = line.split(':')
     _, game_id = game.split()
@@ -12,7 +12,7 @@ for line in text.splitlines():
         for cube_value in cube_set.split(','):
             value, color = cube_value.split()
             cube_values[color].append(int(value))
-    minimum_cubes.append([max(values) for values in cube_values.values()])
-minimum_cubes = np.array(minimum_cubes)
-print(sum(np.where(np.all(minimum_cubes <= [12, 13, 14], axis=1))[0] + 1))
-print(np.prod(minimum_cubes, axis=1).sum())
+    required_cubes.append([max(values) for values in cube_values.values()])
+required_cubes = np.array(required_cubes)
+print(sum(np.where(np.all(required_cubes <= [12, 13, 14], axis=1))[0] + 1))
+print(np.prod(required_cubes, axis=1).sum())
