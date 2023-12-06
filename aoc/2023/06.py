f374f8875c1b74d57781526aff242e374e984cf1
sve <>
2023-12-06 07:51:41 +0200

    refactor 06

--- aoc/2023/06.py
+++ aoc/2023/06.py
@@ -4,9 +4,9 @@ from functools import reduce
 from aoc.problem import Problem
 
 
-def n_ways_to_win(ms, record):
-    D = math.sqrt(ms * ms - 4 * record)
-    x1, x2 = round((ms - D) / 2, 9), round((ms + D) / 2, 9)
+def n_ways_to_win(time, distance):
+    D = math.sqrt(time * time - 4 * distance)
+    x1, x2 = round((time - D) / 2, 9), round((time + D) / 2, 9)
     answer = math.floor(x2) - math.ceil(x1) + 1
     if x1 == math.ceil(x1):
         # If x1 is an integer, then x2 is also an integer. Follows from Vieta's formulas.
