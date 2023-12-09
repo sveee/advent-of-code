48c758044faf2e596ead75becbe04b72897c506d
sve <>
2023-12-09 07:24:59 +0200

    add day 09 part 2

--- aoc/2023/09.py
+++ aoc/2023/09.py
@@ -1,23 +1,24 @@
 from aoc.problem import Problem
 
 
-def extrapolate_value(sequence):
+def extrapolate_value(sequence, last):
     if all(value == 0 for value in sequence):
         return 0
-    next_bottom = extrapolate_value(
-        [value2 - value1 for value1, value2 in zip(sequence, sequence[1:])]
+    prev = extrapolate_value(
+        [value2 - value1 for value1, value2 in zip(sequence, sequence[1:])], last
     )
-    return sequence[-1] + next_bottom
+    return sequence[-1] + prev if last else sequence[0] - prev
 
 
 class Promblem2023_09(Problem):
     def solve(self, text):
-        extrapolated_values = []
-        for line in text.splitlines():
-            extrapolated_values.append(extrapolate_value(list(map(int, line.split()))))
-        self.part1 = sum(extrapolated_values)
-        self.part2 = None
+        sequences = [list(map(int, line.split())) for line in text.splitlines()]
+        self.part1 = sum(
+            [extrapolate_value(sequence, last=True) for sequence in sequences]
+        )
+        self.part2 = sum(
+            [extrapolate_value(sequence, last=False) for sequence in sequences]
+        )
 
 
-Promblem2023_09().check_test()
-Promblem2023_09().submit()
+Promblem2023_09().print_solution()
