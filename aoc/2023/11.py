f6acea866bb9f295a5cbfd83c6878a71f2ba5104
sve <>
2023-12-11 07:55:06 +0200

    add day 11 part 2

--- aoc/2023/11.py
+++ aoc/2023/11.py
@@ -9,7 +9,7 @@ def get_empty_rows(grid):
     }
 
 
-def get_columns_rows(grid):
+def get_empty_columns(grid):
     return {
         column_index
         for column_index in range(len(grid[0]))
@@ -17,30 +17,34 @@ def get_columns_rows(grid):
     }
 
 
+def get_total_distance(grid, scale):
+    galaxies = []
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] == '#':
+                galaxies.append((x, y))
+
+    empty_rows = get_empty_rows(grid)
+    empty_columns = get_empty_columns(grid)
+    total_distance = 0
+    for index, (x1, y1) in enumerate(galaxies):
+        for x2, y2 in galaxies[index + 1 :]:
+            distance = abs(x2 - x1) + abs(y2 - y1)
+            for x in empty_rows:
+                if min(x1, x2) <= x <= max(x1, x2):
+                    distance += scale - 1
+            for y in empty_columns:
+                if min(y1, y2) <= y <= max(y1, y2):
+                    distance += scale - 1
+            total_distance += distance
+    return total_distance
+
+
 class Problem2023_11(Problem):
     def part1(self, text):
         grid = text.splitlines()
+        return get_total_distance(grid, 2)
 
-        galaxies = []
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if grid[x][y] == '#':
-                    galaxies.append((x, y))
-
-        empty_rows = get_empty_rows(grid)
-        empty_columns = get_columns_rows(grid)
-        total_distance = 0
-        for index, (x1, y1) in enumerate(galaxies):
-            for x2, y2 in galaxies[index + 1 :]:
-                distance = abs(x2 - x1) + abs(y2 - y1)
-                for x in empty_rows:
-                    if min(x1, x2) <= x <= max(x1, x2):
-                        distance += 1
-                for y in empty_columns:
-                    if min(y1, y2) <= y <= max(y1, y2):
-                        distance += 1
-                total_distance += distance
-        return total_distance
-
-    def part2(self, text):
-        pass
+    def part2(self, text, scale=1_000_000):
+        grid = text.splitlines()
+        return get_total_distance(grid, scale)
