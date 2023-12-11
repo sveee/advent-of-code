87d8ded1c26749f598a3f4f55787b798656d0a1b
sve <>
2023-12-11 08:27:39 +0200

    remove year, day dependency in problem

--- aoc/problem.py
+++ aoc/problem.py
@@ -1,13 +1,8 @@
-import re
 from abc import abstractmethod
 from typing import Any
 
 
 class Problem:
-    def __init__(self) -> None:
-        year, day = re.findall('\d+', self.__class__.__name__)
-        self.year, self.day = int(year), int(day)
-
     @abstractmethod
     def part1(self, text: str, **kwargs) -> Any:
         pass
