6ddd1ce81cc71f2f8859617460f61a9062270544
sve <>
2023-12-12 08:16:18 +0200

    add day 12 part 2

--- aoc/2023/12.py
+++ aoc/2023/12.py
@@ -1,26 +1,44 @@
+def f(index, prev_record, sizes, records, cache):
+    state = (index, prev_record, sizes)
+    if state in cache:
+        return cache[state]
 
-import re
-from itertools import product
+    if index == len(records):
+        return 1 if sizes == (0,) or sizes == tuple() else 0
 
-def get_n_arrangements(damaged_records, sizes):
+    current_records = [records[index]] if records[index] != '?' else ['.', '#']
     n_arrangements = 0
-    for records in product(*[
-        ['.', '#'] if record == '?' else [record]
-        for record in damaged_records
-    ]):
-        records = re.split('\.+', ''.join(records).strip('.'))
-        if list(map(len, records)) == sizes:
-            n_arrangements += 1
-    return n_arrangements
+    for current_record in current_records:
+        if current_record == '.':
+            if prev_record == '#' and len(sizes) > 0 and sizes[0] == 0:
+                n_arrangements += f(
+                    index + 1, current_record, sizes[1:], records, cache
+                )
+            elif prev_record == '.':
+                n_arrangements += f(index + 1, current_record, sizes, records, cache)
 
+        if current_record == '#' and len(sizes) > 0 and sizes[0] > 0:
+            n_arrangements += f(
+                index + 1, current_record, (sizes[0] - 1,) + sizes[1:], records, cache
+            )
+
+    cache[state] = n_arrangements
+    return n_arrangements
 
 
 def part1(text):
     total_n_arrangements = 0
     for line in text.splitlines():
         records, sizes = line.split()
-        total_n_arrangements += get_n_arrangements(records, list(map(int, sizes.split(','))))
+        sizes = tuple(map(int, sizes.split(',')))
+        total_n_arrangements += f(0, '.', sizes, records, {})
     return total_n_arrangements
 
+
 def part2(text):
-    pass
+    total_n_arrangements = 0
+    for line in text.splitlines():
+        records, sizes = line.split()
+        sizes = tuple(map(int, sizes.split(',')))
+        total_n_arrangements += f(0, '.', 5 * sizes, '?'.join(5 * [records]), {})
+    return total_n_arrangements
