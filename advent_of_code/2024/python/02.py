a15e8817d178d61b725df5d05c392d79e46651b8
sve <svetoslav.karaivanov@gmail.com>
2024-12-02 07:28:17 +0200

    refactor day02 py

--- advent_of_code/2024/python/02.py
+++ advent_of_code/2024/python/02.py
@@ -1,34 +1,23 @@
-
 def is_safe(level):
-    return (
-        all(1<= abs(a-b) <= 3 for a, b in zip(level, level[1:]))
-        and (
-            all(a > b for a, b in zip(level, level[1:]))
-            or
-            all(a < b for a, b in zip(level, level[1:]))
-        )
+    return all(1 <= abs(a - b) <= 3 for a, b in zip(level, level[1:])) and (
+        all(a > b for a, b in zip(level, level[1:]))
+        or all(a < b for a, b in zip(level, level[1:]))
+    )
+
+
+def is_almost_safe(level):
+    return any(
+        is_safe(level[:i] + level[i + 1 :]) for i in range(len(level))
     )
 
 
 def part1(text):
-    total = 0
-    for line in text.splitlines():
-        level = list(map(int, line.split()))
-        total += is_safe(level)
-    return total
+    return sum(is_safe(list(map(int, line.split()))) for line in text.splitlines())
+
 
 def part2(text):
-    total = 0
-    for line in text.splitlines():
-        level = list(map(int, line.split()))
-        safe = False
-        if not is_safe(level):
-            for i in range(len(level)):
-                if is_safe(level[:i] + level[i + 1: ]):
-                    safe = True
-                    break
-        else:
-            safe = True
-        # print(level, safe)
-        total += safe
-    return total
+    return sum(
+        is_safe(level) or is_almost_safe(level)
+        for line in text.splitlines()
+        if (level := list(map(int, line.split())))
+    )
