f710826175f285ca1a99583878522c88c7cd6901
sve <svetoslav.karaivanov@gmail.com>
2024-12-02 07:20:18 +0200

    day 02 part 2 py

--- advent_of_code/2024/python/02.py
+++ advent_of_code/2024/python/02.py
@@ -1,19 +1,34 @@
 
+def is_safe(level):
+    return (
+        all(1<= abs(a-b) <= 3 for a, b in zip(level, level[1:]))
+        and (
+            all(a > b for a, b in zip(level, level[1:]))
+            or
+            all(a < b for a, b in zip(level, level[1:]))
+        )
+    )
+
+
 def part1(text):
     total = 0
     for line in text.splitlines():
         level = list(map(int, line.split()))
-        safe = (
-            all(1<= abs(a-b) <= 3 for a, b in zip(level, level[1:]))
-            and (
-                all(a > b for a, b in zip(level, level[1:]))
-                or
-                all(a < b for a, b in zip(level, level[1:]))
-            )
-        )
-        if safe:
-            total += 1
+        total += is_safe(level)
     return total
 
 def part2(text):
-    pass
+    total = 0
+    for line in text.splitlines():
+        level = list(map(int, line.split()))
+        safe = False
+        if not is_safe(level):
+            for i in range(len(level)):
+                if is_safe(level[:i] + level[i + 1: ]):
+                    safe = True
+                    break
+        else:
+            safe = True
+        # print(level, safe)
+        total += safe
+    return total
