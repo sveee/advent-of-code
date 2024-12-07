4eb41a7186af657aaaac6eca5a26c11e298265fb
sve <svetoslav.karaivanov@gmail.com>
2024-12-07 08:35:20 +0200

    refactor day 07 py

--- advent_of_code/2024/python/07.py
+++ advent_of_code/2024/python/07.py
@@ -1,16 +1,21 @@
-from itertools import product
+def decode(coded_ops, n, base):
+    ops = []
+    for _ in range(n):
+        ops.append(coded_ops % base)
+        coded_ops //= base
+    return ops
 
 
-def can_be_made_true(numbers, total, ops):
+def can_be_made_true(numbers, total, n_ops):
     n = len(numbers)
-    for operations in product(*([ops] * (n - 1))):
+    for coded_ops in range(n_ops ** (n - 1)):
+        ops = decode(coded_ops, n - 1, n_ops)
         result = numbers[0]
-        for i in range(n - 1):
-            if operations[i] == '*':
+        for i, op in enumerate(ops):
+            if op == 0:
                 result *= numbers[i + 1]
-            elif operations[i] == '+':
-                result += numbers[i + 1]
-            elif operations[i] == '||':
+            elif op == 1:
+                result += numbers[i + 1]../advent_of_code/2024/python/07.py
                 result = int(str(result) + str(numbers[i + 1]))
         if result == total:
             return True
@@ -22,7 +27,7 @@ def part1(text):
     for line in text.splitlines():
         lhs, rhs = line.split(': ')
         lhs, rhs = int(lhs), list(map(int, rhs.split(' ')))
-        if can_be_made_true(rhs, lhs, ['*', '+']):
+        if can_be_made_true(rhs, lhs, 2):
             total += lhs
     return total
 
@@ -32,6 +37,6 @@ def part2(text):
     for line in text.splitlines():
         lhs, rhs = line.split(': ')
         lhs, rhs = int(lhs), list(map(int, rhs.split(' ')))
-        if can_be_made_true(rhs, lhs, ['*', '+', '||']):
+        if can_be_made_true(rhs, lhs, 3):
             total += lhs
     return total
