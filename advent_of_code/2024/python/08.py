5bf09dea8b06108b824a065ccd080dccbcfcc84e
sve <svetoslav.karaivanov@gmail.com>
2024-12-08 09:26:35 +0200

    refactor day 8

--- advent_of_code/2024/python/08.py
+++ advent_of_code/2024/python/08.py
@@ -1,52 +1,56 @@
-
 from collections import defaultdict
 from itertools import combinations
+from typing import NamedTuple
 
 
 def within_bounds(x, y, n, m):
     return 0 <= x < n and 0 <= y < m
 
-def part1(text):
-    grid = text.splitlines()
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+
+def get_antennas(grid):
     antennas = defaultdict(list)
     for x, line in enumerate(grid):
         for y, v in enumerate(line):
             if v != '.':
-                antennas[v].append((x, y))
+                antennas[v].append(Point(x, y))
+    return antennas
 
+
+def part1(text):
+    grid = text.splitlines()
+    n, m = len(grid), len(grid[0])
+    antennas = get_antennas(grid)
     locations = set()
     for group in antennas.values():
         for a1, a2 in combinations(group, 2):
-            an1 = 2*a1[0] - a2[0], 2*a1[1] - a2[1]
-            an2 = 2*a2[0] - a1[0], 2*a2[1] - a1[1]
-            if within_bounds(*an1, len(grid), len(grid[0])):
+            an1 = Point(2 * a1.x - a2.x, 2 * a1.y - a2.y)
+            an2 = Point(2 * a2.x - a1.x, 2 * a2.y - a1.y)
+            if within_bounds(an1.x, an1.y, n, m):
                 locations.add(an1)
-            if within_bounds(*an2, len(grid), len(grid[0])):
+            if within_bounds(an2.x, an2.y, n, m):
                 locations.add(an2)
     return len(locations)
-
 
 
 def part2(text):
     grid = text.splitlines()
     n, m = len(grid), len(grid[0])
-    antennas = defaultdict(list)
-    for x, line in enumerate(grid):
-        for y, v in enumerate(line):
-            if v != '.':
-                antennas[v].append((x, y))
-
+    antennas = get_antennas(grid)
     locations = set()
     for group in antennas.values():
         for a1, a2 in combinations(group, 2):
-            d = a2[0] - a1[0], a2[1] - a1[1]
+            d = Point(a2[0] - a1[0], a2[1] - a1[1])
             an = a1
-            while within_bounds(*an, n, m ):
+            while within_bounds(an.x, an.y, n, m):
                 locations.add(an)
-                an = an[0] - d[0], an[1] - d[1]
+                an = Point(an.x - d.x, an.y - d.y)
             an = a2
-            while within_bounds(*an, n, m ):
+            while within_bounds(an.x, an.y, n, m):
                 locations.add(an)
-                an = an[0] + d[0], an[1] + d[1]
+                an = Point(an.x + d.x, an.y + d.y)
     return len(locations)
-
