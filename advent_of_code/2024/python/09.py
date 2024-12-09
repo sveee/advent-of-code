d628dced364fc07eaa17c29b5504901c3aed066d
sve <svetoslav.karaivanov@gmail.com>
2024-12-09 09:16:44 +0200

    2024 day 09 part2

--- advent_of_code/2024/python/09.py
+++ advent_of_code/2024/python/09.py
@@ -1,4 +1,5 @@
 from collections import deque
+from dataclasses import dataclass
 
 
 def part1(text):
@@ -15,5 +16,47 @@ def part1(text):
     return sum(index * value for index, value in enumerate(compressed))
 
 
+@dataclass
+class Fragment:
+    id: int
+    index: int
+    size: int
+
+
+EMPTY = -1
+
+
 def part2(text):
-    pass
+    disk_map = list(map(int, text))
+    disk = []
+    disk_index = 0
+    files, spaces = [], []
+    for index, size in enumerate(disk_map):
+        if index % 2 == 0:
+            disk.extend([index // 2] * size)
+            files.append(Fragment(index // 2, disk_index, size))
+        else:
+            disk.extend([EMPTY] * size)
+            spaces.append(Fragment(-1, disk_index, size))
+        disk_index += size
+
+    for file in sorted(files, key=lambda x: x.id, reverse=True):
+        start = 0
+        while start < file.index:
+            if disk[start] != EMPTY:
+                start += 1
+            else:
+                end = start
+                while end < len(disk) and disk[end] == EMPTY:
+                    end += 1
+                space_size = end - start
+                if file.size <= space_size:
+                    for index in range(start, start + file.size):
+                        disk[index] = file.id
+                    for index in range(file.index, file.index + file.size):
+                        disk[index] = EMPTY
+                start = end
+                if file.size <= space_size:
+                    break
+
+    return sum(index * value for index, value in enumerate(disk) if value >= 0)
