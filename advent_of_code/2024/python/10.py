9e828c49a2d81bb315ad5037640fbfe7f36f3456
sve <svetoslav.karaivanov@gmail.com>
2024-12-10 07:21:03 +0200

    2024 day 10 part2

--- advent_of_code/2024/python/10.py
+++ advent_of_code/2024/python/10.py
@@ -25,6 +25,21 @@ def dfs(x, y, grid):
     return total
 
 
+def dfs2(x, y, grid):
+    if grid[x][y] == '9':
+        return 1
+
+    total = 0
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if (
+            within_bounds(nx, ny, len(grid), len(grid[0]))
+            and int(grid[nx][ny]) - int(grid[x][y]) == 1
+        ):
+            total += dfs2(nx, ny, grid)
+    return total
+
+
 def part1(text):
     grid = text.splitlines()
 
@@ -37,4 +52,11 @@ def part1(text):
 
 
 def part2(text):
-    pass
+    grid = text.splitlines()
+
+    total = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] == '0':
+                total += dfs2(x, y, grid)
+    return total
