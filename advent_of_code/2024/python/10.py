acd93e1715d71ba9ef52d50d1401e21190d15457
sve <svetoslav.karaivanov@gmail.com>
2024-12-10 07:30:53 +0200

    add day 10

--- advent_of_code/2024/python/10.py
+++ advent_of_code/2024/python/10.py
@@ -6,57 +6,50 @@ directions = [
 ]
 
 
-def within_bounds(x, y, n, m):
+def within_bounds(x, y, grid):
+    n, m = len(grid), len(grid[0])
     return 0 <= x < n and 0 <= y < m
 
 
-def dfs(x, y, grid):
+def trailhead_score(x, y, grid):
     if grid[x][y] == '9':
         return {(x, y)}
 
     total = set()
     for dx, dy in directions:
         nx, ny = x + dx, y + dy
-        if (
-            within_bounds(nx, ny, len(grid), len(grid[0]))
-            and int(grid[nx][ny]) - int(grid[x][y]) == 1
-        ):
-            total.update(dfs(nx, ny, grid))
+        if within_bounds(nx, ny, grid) and ord(grid[nx][ny]) - ord(grid[x][y]) == 1:
+            total.update(trailhead_score(nx, ny, grid))
     return total
 
 
-def dfs2(x, y, grid):
+def trailhead_score2(x, y, grid):
     if grid[x][y] == '9':
         return 1
 
     total = 0
     for dx, dy in directions:
         nx, ny = x + dx, y + dy
-        if (
-            within_bounds(nx, ny, len(grid), len(grid[0]))
-            and int(grid[nx][ny]) - int(grid[x][y]) == 1
-        ):
-            total += dfs2(nx, ny, grid)
+        if within_bounds(nx, ny, grid) and ord(grid[nx][ny]) - ord(grid[x][y]) == 1:
+            total += trailhead_score2(nx, ny, grid)
     return total
 
 
 def part1(text):
     grid = text.splitlines()
-
-    total = 0
-    for x in range(len(grid)):
-        for y in range(len(grid[0])):
-            if grid[x][y] == '0':
-                total += len(dfs(x, y, grid))
-    return total
+    return sum(
+        len(trailhead_score(x, y, grid))
+        for x in range(len(grid))
+        for y in range(len(grid[0]))
+        if grid[x][y] == '0'
+    )
 
 
 def part2(text):
     grid = text.splitlines()
-
-    total = 0
-    for x in range(len(grid)):
-        for y in range(len(grid[0])):
-            if grid[x][y] == '0':
-                total += dfs2(x, y, grid)
-    return total
+    return sum(
+        trailhead_score2(x, y, grid)
+        for x in range(len(grid))
+        for y in range(len(grid[0]))
+        if grid[x][y] == '0'
+    )
