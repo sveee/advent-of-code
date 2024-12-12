96f99dcff74baffa7758a84df72db0887ddaa32a
sve <svetoslav.karaivanov@gmail.com>
2024-12-12 09:48:43 +0200

    refactor

--- advent_of_code/2024/python/12.py
+++ advent_of_code/2024/python/12.py
@@ -17,91 +17,90 @@ def get_neigbours(x, y, grid):
     return neigbours
 
 
-def dfs(x, y, cur_id, region_id, grid):
-    region_id[(x, y)] = cur_id
+def ff(x, y, current_id, region_id_map, grid):
+    region_id_map[(x, y)] = current_id
     for nx, ny in get_neigbours(x, y, grid):
-        if (nx, ny) not in region_id and grid[x][y] == grid[nx][ny]:
-            dfs(nx, ny, cur_id, region_id, grid)
+        if (nx, ny) not in region_id_map and grid[x][y] == grid[nx][ny]:
+            ff(nx, ny, current_id, region_id_map, grid)
+
+
+def get_region_id_map(grid):
+    region_id_map = {}
+    current_id = 0
+    for x in range(len(grid)):
+        for y in range(len(grid)):
+            if (x, y) in region_id_map:
+                continue
+            ff(x, y, current_id, region_id_map, grid)
+            current_id += 1
+    return region_id_map
 
 
 def part1(text):
     grid = text.splitlines()
-    region_id = {}
-    cur_id = 0
-    for x in range(len(grid)):
-        for y in range(len(grid)):
-            if (x, y) in region_id:
-                continue
-            dfs(x, y, cur_id, region_id, grid)
-            cur_id += 1
-
+    region_id_map = get_region_id_map(grid)
     area = defaultdict(int)
     parameter = defaultdict(int)
-    for (x, y), rid in region_id.items():
+    for (x, y), rid in region_id_map.items():
         area[rid] += 1
-        p = 4
-        for nx, ny in get_neigbours(x, y, grid):
-            if region_id[(nx, ny)] == rid:
-                p -= 1
-        parameter[rid] += p
+        parameter[rid] += 4 - sum(
+            1 for nx, ny in get_neigbours(x, y, grid) if region_id_map[(nx, ny)] == rid
+        )
     return sum(area[rid] * parameter[rid] for rid in area)
 
 
 def count_top(region):
     region = sorted(region)
-    prev_x, prev_y = None, None
+    prev_x, prev_y = -1, -1
     n_sides = 0
     for x, y in region:
-        # print(x, y, n_sides, prev_x, prev_y)
-        # print(y - 1 == prev_y, x == prev_x, (x - 1, y) not in region)
         if not (y - 1 == prev_y and x == prev_x and (x - 1, y) not in region):
-            if prev_x != None:
+            if prev_x != -1:
                 n_sides += 1
-        prev_x, prev_y = (x, y) if (x - 1, y) not in region else (None, None)
-    # print()
-    if prev_x != None:
+        prev_x, prev_y = (x, y) if (x - 1, y) not in region else (-1, -1)
+    if prev_x != -1:
         n_sides += 1
     return n_sides
 
 
 def count_bottom(region):
     region = sorted(region, key=lambda p: (-p[0], p[1]))
-    prev_x, prev_y = None, None
+    prev_x, prev_y = -1, -1
     n_sides = 0
     for x, y in region:
         if not (y - 1 == prev_y and x == prev_x and (x + 1, y) not in region):
-            if prev_x != None:
+            if prev_x != -1:
                 n_sides += 1
-        prev_x, prev_y = (x, y) if (x + 1, y) not in region else (None, None)
-    if prev_x != None:
+        prev_x, prev_y = (x, y) if (x + 1, y) not in region else (-1, -1)
+    if prev_x != -1:
         n_sides += 1
     return n_sides
 
 
 def count_left(region):
     region = sorted(region, key=lambda p: (p[1], p[0]))
-    prev_x, prev_y = None, None
+    prev_x, prev_y = -1, -1
     n_sides = 0
     for x, y in region:
         if not (y == prev_y and x - 1 == prev_x and (x, y - 1) not in region):
-            if prev_x != None:
+            if prev_x != -1:
                 n_sides += 1
-        prev_x, prev_y = (x, y) if (x, y - 1) not in region else (None, None)
-    if prev_x != None:
+        prev_x, prev_y = (x, y) if (x, y - 1) not in region else (-1, -1)
+    if prev_x != -1:
         n_sides += 1
     return n_sides
 
 
 def count_right(region):
     region = sorted(region, key=lambda p: (-p[1], p[0]))
-    prev_x, prev_y = None, None
+    prev_x, prev_y = -1, -1
     n_sides = 0
     for x, y in region:
         if not (y == prev_y and x - 1 == prev_x and (x, y + 1) not in region):
-            if prev_x != None:
+            if prev_x != -1:
                 n_sides += 1
-        prev_x, prev_y = (x, y) if (x, y + 1) not in region else (None, None)
-    if prev_x != None:
+        prev_x, prev_y = (x, y) if (x, y + 1) not in region else (-1, -1)
+    if prev_x != -1:
         n_sides += 1
     return n_sides
 
@@ -117,17 +116,8 @@ def count_sides(region):
 
 def part2(text):
     grid = text.splitlines()
-    region_id = {}
-    cur_id = 0
-    for x in range(len(grid)):
-        for y in range(len(grid)):
-            if (x, y) in region_id:
-                continue
-            dfs(x, y, cur_id, region_id, grid)
-            cur_id += 1
-
-    regions = defaultdict(set)
-    for (x, y), rid in region_id.items():
-        regions[rid].add((x, y))
-
-    return sum(len(region) * count_sides(region) for region in regions.values())
+    region_id_map = get_region_id_map(grid)
+    regions_by_id = defaultdict(set)
+    for (x, y), rid in region_id_map.items():
+        regions_by_id[rid].add((x, y))
+    return sum(len(region) * count_sides(region) for region in regions_by_id.values())
