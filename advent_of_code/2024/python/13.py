1c6a4ed2fddac6fa3169ee28a0cc4e2204f3fef5
sve <svetoslav.karaivanov@gmail.com>
2024-12-13 08:41:35 +0200

    refactor

--- advent_of_code/2024/python/13.py
+++ advent_of_code/2024/python/13.py
@@ -1,29 +1,50 @@
 import re
+from typing import NamedTuple
+
+
+class Point(NamedTuple):
+    x: int
+    y: int
 
 
 def min_tokens(da, db, p):
+    # da.x, db.x  *   na   =   p.x
+    # da.y, db.y      nb       p.y
 
-    return min(
-        [
-            3 * na + nb
-            for na in range(101)
-            for nb in range(101)
-            if na * da[0] + nb * db[0] == p[0] and na * da[1] + nb * db[1] == p[1]
-        ],
-        default=0,
-    )
+    D = da.x * db.y - da.y * db.x
+    A = p.x  * db.y - p.y  * db.x
+    B = da.x *  p.y - da.y *  p.x
 
+    if D < 0:
+        A *= -1
+        B *= -1
+        D *= -1
 
-def part1(text):
-    total = 0
+    if A % D == 0 and B % D == 0:
+        return 3 * A // D + B // D
+    
+    return 0
+
+def get_machines(text):
+    machines = []
     for machine in text.split('\n\n'):
         button_a, button_b, prize = machine.splitlines()
-        da = tuple(map(int, re.findall('\d+', button_a)))
-        db = tuple(map(int, re.findall('\d+', button_b)))
-        p = tuple(map(int, re.findall('\d+', prize)))
-        total += min_tokens(da, db, p)
-    return total
+        da = Point(*map(int, re.findall('\d+', button_a)))
+        db = Point(*map(int, re.findall('\d+', button_b)))
+        p = Point(*map(int, re.findall('\d+', prize)))
+        machines.append((da, db, p))
+    return machines
 
+def part1(text):
+    return sum(
+        min_tokens(da, db, p)
+        for da, db, p in get_machines(text)
+    )
+
+N = 10000000000000
 
 def part2(text):
-    pass
+    return sum(
+        min_tokens(da, db, Point(p.x+N,p.y+N))
+        for da, db, p in get_machines(text)
+    )
