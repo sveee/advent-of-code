56614e3cf493f4a8d547879189ac4f1bbf427f9c
sve <svetoslav.karaivanov@gmail.com>
2024-12-16 08:16:58 +0200

    2024 day 16 part2

--- advent_of_code/2024/python/16.py
+++ advent_of_code/2024/python/16.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from dataclasses import dataclass
 from queue import PriorityQueue
 
@@ -52,11 +53,61 @@ def part1(text):
 
         if grid[next_position.x][next_position.y] != '#':
             next_state = State(next_position, state.direction)
-            queue.put((priority + 1, next_state))
-            visited.add(next_state)
+            if next_state not in visited:
+                queue.put((priority + 1, next_state))
+                visited.add(next_state)
 
-    raise
+
+def find_value(v, grid):
+    return next(
+        Point(x, y)
+        for x in range(len(grid))
+        for y in range(len(grid[0]))
+        if grid[x][y] == v
+    )
 
 
 def part2(text):
-    pass
+    grid = text.splitlines()
+    s = find_value('S', grid)
+    e = find_value('E', grid)
+    start_state = State(s, Point(0, 1))
+    queue = PriorityQueue()
+    queue.put((0, start_state))
+    visited = {start_state: 0}
+    prev = defaultdict(list)
+
+    while not queue.empty():
+        priority, state = queue.get()
+        for direction in directions:
+            next_state = State(state.position, direction)
+            if next_state not in visited:
+                visited[next_state] = priority + 1000
+                queue.put((visited[next_state], next_state))
+            if priority + 1000 == visited[next_state]:
+                prev[next_state].append(state)
+
+        next_position = Point(
+            state.position.x + state.direction.x,
+            state.position.y + state.direction.y,
+        )
+        if grid[next_position.x][next_position.y] != '#':
+            next_state = State(next_position, state.direction)
+            if next_state not in visited:
+                visited[next_state] = priority + 1
+                queue.put((visited[next_state], next_state))
+            if priority + 1 == visited[next_state]:
+                prev[next_state].append(state)
+
+    end_states = [State(e, direction) for direction in directions]
+    best_end_state = min(end_states, key=lambda x: visited[x])
+    paths = {best_end_state}
+    stack = [best_end_state]
+    while len(stack) > 0:
+        position = stack.pop()
+        for next_position in prev[position]:
+            if next_position not in paths:
+                stack.append(next_position)
+                paths.add(next_position)
+
+    return len({state.position for state in paths})
