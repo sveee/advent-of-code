a423927aea91b3f9e75527b00e4eb5b3d0f2eef5
sve <svetoslav.karaivanov@gmail.com>
2024-12-16 08:24:18 +0200

    refactor

--- advent_of_code/2024/python/16.py
+++ advent_of_code/2024/python/16.py
@@ -1,61 +1,24 @@
 from collections import defaultdict
-from dataclasses import dataclass
 from queue import PriorityQueue
+from typing import NamedTuple
 
 
-@dataclass(frozen=True, order=True)
-class Point:
+class Point(NamedTuple):
     x: int
     y: int
 
 
-@dataclass(frozen=True, order=True)
-class State:
+class State(NamedTuple):
     position: Point
     direction: Point
 
 
-directions = [
+DIRECTIONS = [
     Point(-1, 0),
     Point(1, 0),
     Point(0, -1),
     Point(0, 1),
 ]
-
-
-def part1(text):
-    grid = text.splitlines()
-    s = next(
-        Point(x, y)
-        for x in range(len(grid))
-        for y in range(len(grid[0]))
-        if grid[x][y] == 'S'
-    )
-    start_state = State(s, Point(0, 1))
-    queue = PriorityQueue()
-    queue.put((0, start_state))
-    visited = {start_state}
-
-    while not queue.empty():
-        priority, state = queue.get()
-
-        if grid[state.position.x][state.position.y] == 'E':
-            return priority
-
-        for direction in directions:
-            if (next_state := State(state.position, direction)) not in visited:
-                queue.put((priority + 1000, next_state))
-                visited.add(next_state)
-
-        next_position = Point(
-            state.position.x + state.direction.x, state.position.y + state.direction.y
-        )
-
-        if grid[next_position.x][next_position.y] != '#':
-            next_state = State(next_position, state.direction)
-            if next_state not in visited:
-                queue.put((priority + 1, next_state))
-                visited.add(next_state)
 
 
 def find_value(v, grid):
@@ -67,24 +30,20 @@ def find_value(v, grid):
     )
 
 
-def part2(text):
-    grid = text.splitlines()
-    s = find_value('S', grid)
-    e = find_value('E', grid)
+def dijkstra(s, grid):
     start_state = State(s, Point(0, 1))
     queue = PriorityQueue()
     queue.put((0, start_state))
-    visited = {start_state: 0}
+    distance = {start_state: 0}
     prev = defaultdict(list)
-
     while not queue.empty():
         priority, state = queue.get()
-        for direction in directions:
+        for direction in DIRECTIONS:
             next_state = State(state.position, direction)
-            if next_state not in visited:
-                visited[next_state] = priority + 1000
-                queue.put((visited[next_state], next_state))
-            if priority + 1000 == visited[next_state]:
+            if next_state not in distance:
+                distance[next_state] = priority + 1000
+                queue.put((distance[next_state], next_state))
+            if priority + 1000 == distance[next_state]:
                 prev[next_state].append(state)
 
         next_position = Point(
@@ -93,14 +52,31 @@ def part2(text):
         )
         if grid[next_position.x][next_position.y] != '#':
             next_state = State(next_position, state.direction)
-            if next_state not in visited:
-                visited[next_state] = priority + 1
-                queue.put((visited[next_state], next_state))
-            if priority + 1 == visited[next_state]:
+            if next_state not in distance:
+                distance[next_state] = priority + 1
+                queue.put((distance[next_state], next_state))
+            if priority + 1 == distance[next_state]:
                 prev[next_state].append(state)
 
-    end_states = [State(e, direction) for direction in directions]
-    best_end_state = min(end_states, key=lambda x: visited[x])
+    return distance, prev
+
+
+def part1(text):
+    grid = text.splitlines()
+    s = find_value('S', grid)
+    e = find_value('E', grid)
+    distance, _ = dijkstra(s, grid)
+    return min(distance[State(e, direction)] for direction in DIRECTIONS)
+
+
+def part2(text):
+    grid = text.splitlines()
+    s = find_value('S', grid)
+    e = find_value('E', grid)
+    distance, prev = dijkstra(s, grid)
+    best_end_state = min(
+        [State(e, direction) for direction in DIRECTIONS], key=lambda x: distance[x]
+    )
     paths = {best_end_state}
     stack = [best_end_state]
     while len(stack) > 0:
@@ -109,5 +85,4 @@ def part2(text):
             if next_position not in paths:
                 stack.append(next_position)
                 paths.add(next_position)
-
     return len({state.position for state in paths})
