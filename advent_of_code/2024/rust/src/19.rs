05785c2f8c3add365d48c7645b16c46e2f9bfd34
sve <svetoslav.karaivanov@gmail.com>
2024-12-19 08:22:06 +0200

    day 19 rust

---
+++ advent_of_code/2024/rust/src/19.rs
@@ -0,0 +1,59 @@
+use std::collections::{HashMap, HashSet};
+
+fn n_ways(design: &str, towels: &HashSet<String>, cache: &mut HashMap<String, i64>) -> i64 {
+    if design.is_empty() {
+        return 1;
+    }
+
+    if let Some(&cached) = cache.get(design) {
+        return cached;
+    }
+
+    let mut ans = 0;
+    for i in 1..=design.len() {
+        let prefix = &design[..i];
+        if towels.contains(prefix) {
+            ans += n_ways(&design[i..], towels, cache);
+        }
+    }
+
+    cache.insert(design.to_string(), ans);
+    ans
+}
+
+fn part1(text: &str) -> i64 {
+    let parts: Vec<&str> = text.split("\n\n").collect();
+    let (left, right) = (parts[0], parts[1]);
+
+    let towels: HashSet<String> = left.split(", ").map(|s| s.to_string()).collect();
+    let mut cache = HashMap::new();
+
+    right
+        .lines()
+        .map(|design| {
+            let ways = n_ways(design, &towels, &mut cache);
+            if ways > 0 {
+                1
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+fn part2(text: &str) -> i64 {
+    let parts: Vec<&str> = text.split("\n\n").collect();
+    let (left, right) = (parts[0], parts[1]);
+
+    let towels: HashSet<String> = left.split(", ").map(|s| s.to_string()).collect();
+    let mut cache = HashMap::new();
+
+    right
+        .lines()
+        .map(|design| n_ways(design, &towels, &mut cache))
+        .sum()
+}
+
+fn main() {
+    aoc2024::solve(part1, part2);
+}
