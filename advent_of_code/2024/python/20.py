d454cc69991967e2d7eaab716fc72f9266e842a1
sve <svetoslav.karaivanov@gmail.com>
2024-12-20 08:20:38 +0200

    2024 day 20 part2

--- advent_of_code/2024/python/20.py
+++ advent_of_code/2024/python/20.py
@@ -1,4 +1,5 @@
 from collections import deque
+from typing import NamedTuple
 
 directions = [
     (-1, 0),
@@ -14,6 +15,12 @@ def locate_value(v, grid):
     return next(
         (x, y) for x in range(len(grid)) for y in range(len(grid[0])) if grid[x][y] == v
     )
+
+
+class Node(NamedTuple):
+    x: int
+    y: int
+    level: int
 
 
 def bfs(grid):
@@ -35,25 +42,30 @@ def bfs(grid):
             ):
                 distance[(nx, ny)] = distance[(x, y)] + 1
                 queue.append((nx, ny))
-    return distance[e]
+    shortest_path = [Node(x, y, level) for (x, y), level in distance.items()]
+    return sorted(shortest_path, key=lambda x: x.level)
+
+
+def get_distance(node1, node2):
+    return abs(node1.x - node2.x) + abs(node1.y - node2.y)
+
+
+def n_cheats(grid, size, at_least):
+    shortest_path = bfs(grid)
+    total_cheats = 0
+    for index, node1 in enumerate(shortest_path):
+        for node2 in shortest_path[index + 1 :]:
+            distance = get_distance(node1, node2)
+            if distance <= size and node2.level - node1.level - distance >= at_least:
+                total_cheats += 1
+    return total_cheats
 
 
 def part1(text, at_least=100):
     grid = list(map(list, text.splitlines()))
-
-    distance = bfs(grid)
-    total_cheats = 0
-    for x in range(1, len(grid) - 1):
-        for y in range(1, len(grid[0]) - 1):
-            if grid[x][y] == WALL:
-                grid[x][y] = '.'
-                saved_time = distance - bfs(grid)
-                if saved_time >= at_least:
-                    total_cheats += 1
-                grid[x][y] = '#'
-
-    return total_cheats
+    return n_cheats(grid, 2, at_least)
 
 
-def part2(text):
-    pass
+def part2(text, at_least=100):
+    grid = list(map(list, text.splitlines()))
+    return n_cheats(grid, 20, at_least)
