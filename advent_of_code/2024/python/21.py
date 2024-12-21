c19f98f56116ba5bb109995d258a00043c25fefd
sve <svetoslav.karaivanov@gmail.com>
2024-12-21 10:36:52 +0200

    2024 day 21 part2

--- advent_of_code/2024/python/21.py
+++ advent_of_code/2024/python/21.py
@@ -40,57 +40,39 @@ def shortest_paths(start, end, keypad):
     return paths
 
 
-def f(keypad, prev_button, current_button):
-    paths = shortest_paths(keypad, prev_button, current_button)
-    if keypad == 3:
-        return len(paths[0]) + 1
-    ans = 10**10
-    for path in paths:
-        for next_start, next_end in zip('A' + path, path):
-            ans = min(ans, f(keypad + 1, next_start, next_end) + len(path))
-    return ans
-
-
-# def type_in(code, keypad):
-#     if keypad == 3:
-#         return [code]
-#     seqs = ['']
-#     for start, end in zip('A' + code, code):
-#         seqs = [
-#             prefix + suffix
-#             for prefix in seqs
-#             for path in shortest_paths(start, end, keypad)
-#             for suffix in  type_in(path + 'A', keypad + 1)
-#         ]
-#         min_len = min(map(len, seqs))
-#         seqs = [
-#             seq
-#             for seq in seqs
-#             if len(seq) == min_len
-#         ]
-#     return seqs
-
-
-def shortest_length(code, keypad):
-    if keypad == 3:
+def shortest_length(code, keypad, n, cache):
+    if keypad == n:
         return len(code)
+
+    state = code, keypad
+    if state in cache:
+        return cache[state]
 
     ans = 0
     for start, end in zip('A' + code, code):
         ans += min(
-            shortest_length(path + 'A', keypad + 1)
+            shortest_length(path + 'A', keypad + 1, n, cache)
             for path in shortest_paths(start, end, keypad)
         )
+    cache[state] = ans
     return ans
 
 
 def part1(text):
     tests1()
-    return sum(shortest_length(code, 0) * int(code[:-1]) for code in text.splitlines())
+    cache = {}
+    return sum(
+        shortest_length(code, 0, 3, cache) * int(code[:-1])
+        for code in text.splitlines()
+    )
 
 
 def part2(text):
-    pass
+    cache = {}
+    return sum(
+        shortest_length(code, 0, 26, cache) * int(code[:-1])
+        for code in text.splitlines()
+    )
 
 
 def tests1():
@@ -100,13 +82,8 @@ def tests1():
     assert shortest_paths('2', '4', 0) == ['<^', '^<']
 
     # assert '<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A' in type_in('029A', 0)
-    assert shortest_length('029A', 0) == 68
-    assert shortest_length('980A', 0) == 60
-    assert shortest_length('179A', 0) == 68
-    assert shortest_length('456A', 0) == 64
-    assert shortest_length('379A', 0) == 64
-
-    # assert False
-    #  assert shortest_length('<A', 2) == len('v<<A>>^A')
-    #  assert shortest_length('<', 1) == len('<vA<AA>>^A')
-    #  assert shortest_length('<A', 1) == len('<vA<AA>>^AvAA<^A>A')
+    assert shortest_length('029A', 0, 3, {}) == 68
+    assert shortest_length('980A', 0, 3, {}) == 60
+    assert shortest_length('179A', 0, 3, {}) == 68
+    assert shortest_length('456A', 0, 3, {}) == 64
+    assert shortest_length('379A', 0, 3, {}) == 64
