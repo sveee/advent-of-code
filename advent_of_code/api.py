d01feeda4bf7acefe465b42ccebc8cc69cb584b1
sve <svetoslav.karaivanov@gmail.com>
2024-12-22 13:28:59 +0200

    refactor api

--- advent_of_code/api.py
+++ advent_of_code/api.py
@@ -1,43 +1,42 @@
-import os
 from typing import Any, Dict
 
 import requests
 from bs4 import BeautifulSoup
 
-AOC_SESSION = os.environ['AOC_SESSION']
 
+class AOCApi:
+    def __init__(self, session_id: str) -> None:
+        self._session_id = session_id
 
-def get_request(url: str) -> str:
-    return requests.get(
-        url,
-        cookies=dict(session=AOC_SESSION),
-    ).text
+    def get_request(self, url: str) -> str:
+        return requests.get(
+            url,
+            cookies=dict(session=self._session_id),
+        ).text
 
+    def post_request(self, url: str, data: Dict[str, Any]) -> str:
+        return requests.post(
+            url,
+            data=data,
+            cookies=dict(session=self._session_id),
+        ).text
 
-def post_request(url: str, data: Dict[str, Any]) -> str:
-    return requests.post(
-        url,
-        data=data,
-        cookies=dict(session=AOC_SESSION),
-    ).text
+    def get_problem_input_data(self, year: int, day: int) -> str:
+        input_text = self.get_request(
+            f'https://adventofcode.com/{year}/day/{day}/input'
+        )
+        if input_text.endswith('\n'):
+            input_text = input_text[:-1]
+        return input_text
 
+    def submit_solution(self, answer: str, level: int, year: int, day: int) -> str:
+        return self.post_request(
+            f'https://adventofcode.com/{year}/day/{day}/answer',
+            {'level': level, 'answer': answer},
+        )
 
-def get_problem_input_data(year: int, day: int) -> str:
-    input_text = get_request(f'https://adventofcode.com/{year}/day/{day}/input')
-    if input_text.endswith('\n'):
-        input_text = input_text[:-1]
-    return input_text
-
-
-def submit_solution(answer: str, level: int, year: int, day: int) -> str:
-    return post_request(
-        f'https://adventofcode.com/{year}/day/{day}/answer',
-        {'level': level, 'answer': answer},
-    )
-
-
-def get_problem_soup(year: int, day: int) -> str:
-    return BeautifulSoup(
-        get_request(f'https://adventofcode.com/{year}/day/{day}'),
-        features='html.parser',
-    )
+    def get_problem_soup(self, year: int, day: int) -> str:
+        return BeautifulSoup(
+            self.get_request(f'https://adventofcode.com/{year}/day/{day}'),
+            features='html.parser',
+        )
