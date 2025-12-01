f2e2e7396c33b7ab07a40fb18b2e53fda84d027e
sve <svetoslav.karaivanov@gmail.com>
2025-12-01 08:05:55 +0200

    add 2025 day2.2 cpp

--- advent_of_code/2025/cpp/01.cpp
+++ advent_of_code/2025/cpp/01.cpp
@@ -27,7 +27,28 @@ const string part1(const string &input) {
 }
 
 const string part2(const string &input) {
-    return "";
+    vector<string> lines =
+        input | views::split('\n') |
+        views::transform([](auto &&rng) { return string(rng.begin(), rng.end()); }) |
+        ranges::to<vector<string>>();
+    int dial = 50;
+    int password = 0;
+    for (auto &line : lines) {
+        int direction = (line[0] == 'L') ? -1 : 1;
+        int value = stoi(line.substr(1));
+        for (int k = 0; k < value; ++k) {
+            dial += direction;
+            if (dial >= 100) {
+                dial = 0;
+            } else if (dial < 0) {
+                dial = 99;
+            }
+            if (dial == 0) {
+                ++password;
+            }
+        }
+    }
+    return to_string(password);
 }
 
 int main(int argc, char *argv[]) {
