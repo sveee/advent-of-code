3cae30de1c446729a0932b8bec227c0e414df8a8
sve <svetoslav.karaivanov@gmail.com>
2025-12-04 22:52:02 +0200

    add 2021 03 p2 py

--- advent_of_code/2021/python/03.py
+++ advent_of_code/2021/python/03.py
@@ -9,5 +9,22 @@ def part1(text):
     return gamma * epsilon
 
 
+def get_rating(lines, co2_scrubber=False):
+    n = len(lines[0])
+    for i in range(n):
+        if len(lines) == 1:
+            break
+        column = [line[i] for line in lines]
+        n_ones = sum(1 for value in column if value == '1')
+        n_zeros = len(lines) - n_ones
+        if co2_scrubber:
+            target_bit = '0' if 2 * n_zeros <= len(lines) else '1'
+        else:
+            target_bit = '1' if 2 * n_ones >= len(lines) else '0'
+        lines = [line for line in lines if line[i] == target_bit]
+    return int(lines[0], 2)
+
+
 def part2(text):
-    pass
+    lines = text.splitlines()
+    return get_rating(lines, co2_scrubber=False) * get_rating(lines, co2_scrubber=True)
